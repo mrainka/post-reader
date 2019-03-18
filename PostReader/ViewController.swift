d83ef4142bf60d57a88054efb5a4c7a8109f2838
Marcin Rainka <marcin.rainka@gmail.com>
2019-03-18 20:53:21 +0100

    Add initial project

---
+++ PostReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostReader
+//
+//  Created by Marcin Rainka @Home on 18/03/2019.
+//  Copyright © 2019 Marcin Rainka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
