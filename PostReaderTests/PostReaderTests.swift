744b0f1a601a04e8ef675941699a60e234d5e382
Marcin Rainka <marcin.rainka@gmail.com>
2019-03-23 15:28:39 +0100

    Add unit test target

---
+++ PostReaderTests/PostReaderTests.swift
@@ -0,0 +1,11 @@
+//
+//  PostReaderTests.swift
+//  PostReaderTests
+//
+//  Created by Marcin Rainka @Home on 23/03/2019.
+//  Copyright © 2019 Marcin Rainka. All rights reserved.
+//
+
+import XCTest
+
+final class PostReaderTests: XCTestCase {}
